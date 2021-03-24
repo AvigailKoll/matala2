4ba34626c14953cb09fe43f85f60fedffa1accb7
AvigailKoll <avigailkollmann@gmail.com>
2021-03-24 22:38:43 +0200

    first

---
+++ text.py
@@ -0,0 +1,29 @@
+def revword(word):
+	word=word.lower()
+	x=len(word)-1
+	drow=word[x]
+	x=x-1
+	while x>=0:
+		drow=drow+word[x]
+		x=x-1
+	return drow
+
+def countword():
+	file=input('Enter file: ')
+	try: 
+		file=open(file, 'r').readlines()
+	except: 
+		print('file can not be open')
+		quite()
+	counter=0
+	word=file[0].strip()
+	for line in file:
+		words_line=line.split()
+		for words in words_line:
+			name=revword(words)
+			if name==word:
+				counter=counter+1
+		
+	if word!=revword(word):
+		counter=counter+1
+	return counter	
